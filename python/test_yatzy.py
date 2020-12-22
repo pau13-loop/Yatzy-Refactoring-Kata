1c3ad831d55346435f762438b710ab2731180d0f
pau <pllinas@cifpfbmoll.eu>
2020-12-22 22:08:15 +0100

    test: :white_check_mark: We add some new tests for one_pair and double_pair functions

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -19,14 +19,14 @@ def test_yatzy_scores_50():
     assert 0 == Yatzy.yatzy([6, 6, 6, 6, 3])
 
 
-def test_1s():
+def test_ones():
     assert Yatzy.ones(1, 2, 3, 4, 5) == 1
     assert 2 == Yatzy.ones(1, 2, 1, 4, 5)
     assert 0 == Yatzy.ones(6, 2, 2, 4, 5)
     assert 4 == Yatzy.ones(1, 2, 1, 1, 1)
 
 
-def test_2s():
+def test_twos():
     assert 4 == Yatzy.twos(1, 2, 3, 2, 6)
     assert 10 == Yatzy.twos(2, 2, 2, 2, 2)
 
@@ -36,7 +36,7 @@ def test_threes():
     assert 12 == Yatzy.threes(2, 3, 3, 3, 3)
 
 
-def test_fours_test():
+def test_fours():
     assert 12 == Yatzy.fours(4, 4, 4, 5, 5)
     assert 8 == Yatzy.fours(4, 4, 5, 5, 5)
     assert 4 == Yatzy.fours(4, 5, 5, 5, 5)
@@ -48,22 +48,35 @@ def test_fives():
     assert 20 == Yatzy.fives(4, 5, 5, 5, 5)
 
 
-def test_sixes_test():
+def test_sixes():
     assert 0 == Yatzy.sixes(4, 4, 4, 5, 5)
     assert 6 == Yatzy.sixes(4, 4, 6, 5, 5)
     assert 18 == Yatzy.sixes(6, 5, 6, 6, 5)
 
 
 def test_one_pair():
-    assert 6 == Yatzy.score_pair(3, 4, 3, 5, 6)
-    assert 10 == Yatzy.score_pair(5, 3, 3, 3, 5)
-    assert 12 == Yatzy.score_pair(5, 3, 6, 6, 5)
+    assert 6 == Yatzy.one_pair(3, 4, 3, 5, 6)
+    assert 10 == Yatzy.one_pair(5, 3, 3, 3, 5)
+    assert 12 == Yatzy.one_pair(5, 3, 6, 6, 5)
+    # Adding some more tests to make sure the new refactor works
+    # Puting a pair in each extreme
+    assert 2 == Yatzy.one_pair(1, 2, 3, 4, 1)
+    # Adding a pair at the last two positions to check the index is not out of range
+    assert 6 == Yatzy.one_pair(1, 2, 4, 3, 3)
+    # Non possible pairs
+    assert 0 == Yatzy.one_pair(1, 2, 3, 4, 5)
+    # A threesome and a pair, should get just the pair of 2 that equal 4
+    assert 4 == Yatzy.one_pair(1, 1, 2, 2, 1)
 
 
 def test_two_Pair():
     assert 16 == Yatzy.two_pair(3, 3, 5, 4, 5)
     assert 18 == Yatzy.two_pair(3, 3, 6, 6, 6)
     assert 0 == Yatzy.two_pair(3, 3, 6, 5, 4)
+    # A pair in each extreme
+    assert 16 == Yatzy.two_pair(3, 3, 1, 5, 5)
+    # A non possible pairs
+    assert 0 == Yatzy.two_pair(1, 2, 3, 4, 5)
 
 
 def test_three_of_a_kind():
@@ -72,7 +85,7 @@ def test_three_of_a_kind():
     assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 5)
 
 
-def test_four_of_a_knd():
+def test_four_of_a_kind():
     assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 5)
     assert 20 == Yatzy.four_of_a_kind(5, 5, 5, 4, 5)
     assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 3)
