15e4809892ff60853041917ccade1064c4f07535
pau <pllinas@cifpfbmoll.eu>
2020-12-24 02:19:57 +0100

    refactor: :recycle: We refactor the last functions of the code "straights", "poker" and a "full house"

--- python/yatzy.py
+++ python/yatzy.py
@@ -8,7 +8,6 @@ class Yatzy:
 
 # Check that all the faces of the dice are equal to be able to make a Yatzy
 
-
     @staticmethod
     def yatzy(dice):
         for die in dice:
@@ -79,13 +78,14 @@ class Yatzy:
                 count += 6
         return count
 
-# We made an iterable list of the different dices, we make an empty list where we will add the possible pairs
-# We made a variable to keep control of the number of the pair that we are actually working on
-# We start a loop to look through all the dices
-# FIRST CONDITIONAL "if", If the dice count is equal 2 and is equal or bigger than the current pair we are working with, we set up the new value of the current pair
-# SECOND CONDITIONAL, if the account of the dice in the list of possible pairs is grather than 0 we append
-# the value and if is not we clear the list because it menas is a bigger value than the value we had before
-# and after clear the list we append the new bigger value of a possible pair
+# We made an iterable list of the different dices, we made an empty list where we will add the possible
+# pairs. We made a variable to keep control of the number of the pair that we are actually working on. We
+# start a loop to look through all the dices. The FIRST CONDITIONAL "if", if the dice count is equal 2 and
+# the dice is equal or bigger than the current pair we are working with, we set up the new value of the
+# current pair. The SECOND CONDITIONAL "if", if the account of the dice in the list of possible pairs is
+# grather than 0 we append the dice value and if is not we clear the list because it menas is a bigger
+# value than the value we had before and after clear the list we append the new bigger value of a possible
+# pair
     @staticmethod
     def one_pair(d1,  d2,  d3,  d4,  d5):
         all_dice = [d1,  d2,  d3,  d4,  d5]
@@ -101,12 +101,8 @@ class Yatzy:
                     possible_pairs.append(dice)
         return sum(possible_pairs)
 
-# We made an iterable list of the different dices,  we make an empty list where we will add the pairs
-# We start a loop to look though all the dices, the FIRST CONDITIONAL help to check that we are working minum
-# with a pair and the SECOND CONDITIONAL just help to make sure that you are just going to add pairs and
-# nothing over that
-# When we finish the loop we check that we have more than a pair in our list, is is the case we give back the
-# sumative of the pairs if is the opposite we just give back a 0
+# We made an iterable list of the different dices, we make an empty list where we will add the pairs.
+# We start a loop to look though all the dices, the FIRST CONDITIONAL help to check that we are working minum with a pair and the SECOND CONDITIONAL just help to make sure that you are just going to add pairs and nothing over that. When we finish the loop we check that we have more than a pair in our list, if is the case we give back the sumative of the pairs if is the opposite we just give back a 0
     @staticmethod
     def two_pair(d1,  d2,  d3,  d4,  d5):
         all_dice = [d1,  d2,  d3,  d4,  d5]
@@ -120,10 +116,10 @@ class Yatzy:
         else:
             return 0
 
-# We made an iterable list of the different dices,  we make an empty list where we will add the pairs. We
-# start a loop to look through all the dices. If the count of the dice number is equal or bigger to 3 AND the
-# account of the same number dice inside the threesome list is minor than 3, we append the dice number to the
-# threesome list. Finally we return the sumative of the threesome
+# We made an iterable list of the different dices, we make an empty list where we will add the threesome.
+# We start a loop to look through all the dices. If the count of the dice with the same number is equal or
+# bigger to 3 AND the account of the same number dice inside the threesome list is minor than 3, we append
+# the dice to the threesome list. Finally we return the sumative of the list called threesome
     @staticmethod
     def three_of_a_kind(d1,  d2,  d3,  d4,  d5):
         all_dice = [d1,  d2,  d3,  d4,  d5]
@@ -133,78 +129,52 @@ class Yatzy:
                 threesome.append(dice)
         return sum(threesome)
 
+# We made an iterable list of the different dices, we make an empty list where we will add the four dice of
+# same number. We start a loop to look through all the dices. If the count of the dice with the same number
+# is equal or bigger to 4 AND the account of the same number dice inside the poker list is minor than 4, we
+# append the dice to the poker list. Finally we return the sumative of the list called poker
     @staticmethod
-    def four_of_a_kind(_1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
-        return 0
+    def four_of_a_kind(d1,  d2,  d3,  d4,  d5):
+        all_dice = [d1,  d2,  d3,  d4,  d5]
+        poker = []
+        for dice in all_dice:
+            if all_dice.count(dice) >= 4 and poker.count(dice) < 4:
+                poker.append(dice)
+        return sum(poker)
 
+# We made an iterable list of the different dices. And we create a conditional where we sort the list to have it in numerical order from 1 to 5 (in case is a small straight) and we check with a boolean if it's the same than a list from 1 to 5, in case it's true we just return a sumative of all numbers and in case is false we return a 0
     @staticmethod
     def smallStraight(d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-                tallies[4] == 1):
-            return 15
-        return 0
-
-    @staticmethod
-    def largeStraight(d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1
-                and tallies[5] == 1):
-            return 20
-        return 0
-
-    @staticmethod
-    def fullHouse(d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2):
-                _2 = True
-                _2_at = i+1
-
-        for i in range(6):
-            if (tallies[i] == 3):
-                _3 = True
-                _3_at = i+1
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
+        all_dice = [d1,  d2,  d3,  d4,  d5]
+        if sorted(all_dice) == list(range(1, 6)):
+            return sum(all_dice)
         else:
             return 0
+
+# We made an iterable list of the different dices. And we create a conditional where we sort the list to
+# have in numerical order from 2 to 6 (in case is a large straight) and we check with a boolean if it's
+# the same than a list from 2 to 6, in case it's true we just return a sumative of all numbers and in case
+# is false we return a 0
+    @staticmethod
+    def largeStraight(d1,  d2,  d3,  d4,  d5):
+        all_dice = [d1,  d2,  d3,  d4,  d5]
+        if sorted(all_dice) == list(range(2, 7)):
+            return sum(all_dice)
+        else:
+            return 0
+
+# We made a list with all the dice values. We made a loop to look through all the dice numbers. If the
+# dice account is equal to 3 or to 2 means that we have a threesome or a pair and we just want to continue
+# looping through all the dices, in case gives back a "False" result means that we are not having a
+# threesome and a pair to be able to make a full house, in this case we just return a 0. If it finish the
+# loop without get back a "False" means that we have a full house and we return a sumative of the all_dice
+# list
+    @staticmethod
+    def fullHouse(d1, d2, d3, d4, d5):
+        all_dice = [d1,  d2,  d3,  d4,  d5]
+        for dice in all_dice:
+            if all_dice.count(dice) == 3 or all_dice.count(dice) == 2:
+                continue
+            else:
+                return 0
+        return sum(all_dice)
